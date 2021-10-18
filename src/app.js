d9f0bd542a9ac0c25e85cae80092a6821bd0b145
Berkan Çelik <celikberkan1@outlook.com>
2021-10-19 01:00:54 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,30 @@
+console.log("Merhaba Kodlama.io") 
+
+// let dolarYesterday = 9.20
+
+// // Js type safe değildir.
+// let dollarToday = 9.20
+// {cl
+//     let dolarDun = 9.20
+// }
+
+// console.log(dolarYesterday)
+
+// const euroDun = 11.2
+// euroYesterday =11
+
+// console.log(euroYesterday)
+ 
+// let users = getUsersFromApi()
+
+let housingLoans = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredileri"]
+// console.log(housingLoans)
+
+
+
+for(let i = 0;i<housingLoans.length;i++){
+    console.log("<li>"+housingLoans[i]+"</li>")
+
+
+}
+console.log("</ul>")
